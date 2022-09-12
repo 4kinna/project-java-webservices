114fe866161de0c277b5216bab2756f2ce505b4f
Annika <a.staalhandske@gmail.com>
2022-09-12 14:52:00 +0200

    initial setup

---
+++ src/main/java/com/example/projectjavawebservices/ProjectJavaWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectjavawebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJavaWebservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectJavaWebservicesApplication.class, args);
+    }
+
+}
